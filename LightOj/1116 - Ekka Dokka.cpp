fcb8cbca864015bbf60a3dbfa0ffd0496ba92e41
Mahmood Al Rayhan <MhmdRyhn@users.noreply.github.com>
2018-05-08 21:49:38 +0600

    accepted

--- LightOj/1116 - Ekka Dokka.cpp
+++ LightOj/1116 - Ekka Dokka.cpp
@@ -1,29 +1,34 @@
-
 #include<cstdio>
 #include<cmath>
 using namespace std;
-
+ 
 typedef long long ll;
 int main()
 {
     int t;
     ll n;
     scanf("%d",&t);
-
+ 
     for(int i=0; i<t; i++)
     {
         ll j;
         scanf("%lld",&n);
-
-        if(n%2)
+ 
+        if((n%2)==1)
             printf("Case %d: Impossible\n",i+1);
         else
         {
-
-            printf("Case %d: %lld %lld\n",i+1,1ll,n);
+        	ll odd = n/2;
+        	ll even = 2;
+        	while((odd%2)==0)
+        	{
+        		odd/=2;
+        		even*=2;
+        	}
+            printf("Case %d: %lld %lld\n",i+1,odd,even);
         }
     }
-
-
+ 
+ 
     return 0;
 }
