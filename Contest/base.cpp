120106e8c0b736ab932503b5e32d3736e2cd1d5f
Mahmood Al Rayhan <MhmdRyhn@users.noreply.github.com>
2019-02-23 19:54:48 +0600

    created base for c++ programming

---
+++ Contest/base.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+#define for02n(n) for(ll i=0; i<n ;i++)
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+    
+    
+
+
+    return 0;
+}
+
